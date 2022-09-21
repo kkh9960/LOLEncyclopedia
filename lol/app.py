acae0d2c3eebe689071e62d4dddf23f182a406f7
Kwu Hyen Kwak <kkjh9960@gmail.com>
2022-09-21 12:26:54 +0900

    다시 업로드

--- lol/app.py
+++ lol/app.py
@@ -1,27 +1,75 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
 import requests
 
+from datetime import datetime
+
 app = Flask(__name__)
+
+client = MongoClient('43.200.181.231', 27017, username="test", password="test")
+db = client.lol
 
 
 @app.route('/')
 def main():
-    myname = "Sparta"
-    return render_template("index.html", name = myname)
+    # DB에서 저장된 단어 찾아서 HTML에 나타내기
+    names = list(db.lol.find({}, {"_id": False}))
+    return render_template("index.html", names=names)
 
+@app.route('/lol', methods=['GET'])
+def show_diary():
+    lols = list(db.lol.find({}, {'_id': False}))
+    return jsonify({'all_lol': lols})
+
+@app.route('/lol', methods=['POST'])
+def save_diary():
+    title_receive = request.form['title_give']
+    name_receive = request.form['name_give']
+    position_receive = request.form['position_give']
+    content_receive = request.form['content_give']
+    star_receive = request.form['star_give']
+
+    file = request.files["file_give"]
+
+    extension = file.filename.split('.')[-1]
+
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    filename = f'file-{mytime}'
+
+    save_to = f'static/{filename}.{extension}'
+    file.save(save_to)
+
+    doc = {
+        'title': title_receive,
+        'name': name_receive,
+        'position': position_receive,
+        'content': content_receive,
+        'star': star_receive,
+        'file': f'{filename}.{extension}'
+    }
+    
+    db.lol.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
 
 @app.route('/detail')
 def detail():
-    r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-    #requests에서 겟 요청을 보냄
-    response = r.json()
-    #제이슨형태로 만들어줘
-    rows = response['RealtimeCityAir']['row']
-    #rows에 저걸 보내줘
-    word_recevie = request.args.get("word_give")
-    print(word_recevie)
-    return render_template("detail.html", rows=rows)
-    #앞에 rows는 변수선언 뒤에 rows는 위에 리스폰 리얼타임시티 로우를 보냄
+    # API에서 단어 뜻 찾아서 결과 보내기
+    return render_template("detail.html")
+
+
+@app.route('/api/save_word', methods=['POST'])
+def save_word():
+    # 단어 저장하기
+    return jsonify({'result': 'success', 'msg': '단어 저장'})
+
+
+@app.route('/api/delete_word', methods=['POST'])
+def delete_word():
+    # 단어 삭제하기
+    return jsonify({'result': 'success', 'msg': '단어 삭제'})
 
 
 if __name__ == '__main__':
